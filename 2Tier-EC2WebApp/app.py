bea035287acee5f42ce1f7816eeb63748af07c4d
Ben <benbrowne7@icloud.com>
2026-02-16 14:53:39 +0000

    move to aws secrets manager

--- 2Tier-EC2WebApp/app.py
+++ 2Tier-EC2WebApp/app.py
@@ -4,12 +4,43 @@ from datetime import datetime
 import os
 from dotenv import load_dotenv
 from sqlalchemy import inspect
+import boto3
+from botocore.exceptions import ClientError
+import json
+
+def get_secret():
+
+    secret_name = "public/postgresdb"
+    region_name = "eu-north-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = json.loads(get_secret_value_response['SecretString'])
+    password = secret['password'] 
+    host = secret['host']
+
+    return password, host
 
 load_dotenv('postgres.env')
+password, host = get_secret()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-secret-key')
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{password}@{host}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
